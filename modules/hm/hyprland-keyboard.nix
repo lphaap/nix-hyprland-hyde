0a30fe96d4ea5983c6440e3efe11a2d3739c518e
Lassi Haapala <lassi.p.haapala@gmail.com>
2025-04-23 17:59:22 +0300

    0.0.4

--- modules/hm/hyprland-keyboard.nix
+++ modules/hm/hyprland-keyboard.nix
@@ -6,13 +6,16 @@
 }:
 
 {
-  # Hyprland-specific keyboard configuration
-  wayland.windowManager.hyprland = {
-    settings = {
-      input = {
-        kb_layout = "fi";
-        kb_options = "caps:escape";
-      };
-    };
-  };
+  # Create a custom keyboard config file
+  home.file.".config/hypr/hyprland.conf.d/keyboard.conf".text = ''
+    input {
+        kb_layout = fi
+        kb_variant =
+        kb_model =
+        kb_options = caps:escape
+        kb_rules =
+        follow_mouse = 1
+        sensitivity = 0
+    }
+  '';
 }
