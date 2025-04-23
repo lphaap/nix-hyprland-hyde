815743e58471ea91a9b64f22a8bafe8923525f99
Lassi Haapala <lassi.p.haapala@gmail.com>
2025-04-23 14:35:05 +0300

    0.0.4

--- modules/hm/hyprland-keyboard.nix
+++ modules/hm/hyprland-keyboard.nix
@@ -6,22 +6,13 @@
 }:
 
 {
-  hydenix.hm.hyprland = {
-    # Add direct config options to the Hyprland config
-    extraConfig = ''
-      # Set keyboard layout explicitly
-      input {
-          kb_layout = fi
-          kb_variant =
-          kb_model =
-          kb_options = caps:escape
-          kb_rules =
-          follow_mouse = 1
-          touchpad {
-              natural_scroll = false
-          }
-          sensitivity = 0
-      }
-    '';
+  # Hyprland-specific keyboard configuration
+  wayland.windowManager.hyprland = {
+    settings = {
+      input = {
+        kb_layout = "fi";
+        kb_options = "caps:escape";
+      };
+    };
   };
 }
