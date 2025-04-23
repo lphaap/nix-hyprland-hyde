962ee1fd2e4a3fe8694f42b1e5a7dc65c58d739d
Lassi Haapala <lassi.p.haapala@gmail.com>
2025-04-23 18:30:05 +0300

    0.0.4

---
+++ modules/hm/hyprland.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+	# In modules/hm/default.nix or a new file
+	home.file.".config/hypr/userprefs.conf".text = ''
+	# Your existing system config plus your keyboard settings
+	input {
+    		kb_layout = fi
+    		kb_variant =
+    		kb_model =
+    		kb_options = caps:escape
+    		kb_rules =
+    		follow_mouse = 1
+    		sensitivity = 0
+	}
+'';
+}
