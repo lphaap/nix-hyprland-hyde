f7813d0eb2d19d30d7828fc4ab31d92737af2c86
Lassi Haapala <lassi.p.haapala@gmail.com>
2025-04-25 14:46:34 +0300

    0.1.4

--- modules/hm/php.nix
+++ modules/hm/php.nix
@@ -9,7 +9,6 @@
   home.packages = with pkgs; [
     # PHP 8.4 (latest version)
     php84
-    php84Packages.composer
     
     # PHP Extensions
     php84Extensions.pdo
@@ -25,5 +24,39 @@
     # Encryption/Hashing Extensions
     php84Extensions.openssl
     php84Extensions.sodium
+    
+    # Development tools
+    php84Packages.php-cs-fixer
+    php84Packages.phpstan
+    php84Packages.psalm
   ];
+  
+  # Install Composer separately through a shell script to avoid the LICENSE collision
+  home.file.".local/bin/composer-install.sh" = {
+    text = ''
+      #!/bin/sh
+      # Download the latest Composer and install it to ~/.local/bin/composer
+      EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+      php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+      ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+      if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+      then
+          >&2 echo 'ERROR: Invalid installer checksum'
+          rm composer-setup.php
+          exit 1
+      fi
+
+      php composer-setup.php --quiet --install-dir=$HOME/.local/bin --filename=composer
+      RESULT=$?
+      rm composer-setup.php
+      exit $RESULT
+    '';
+    executable = true;
+  };
+  
+  # Create directories needed
+  home.file.".local/bin/.keep" = {
+    text = "";
+  };
 }
