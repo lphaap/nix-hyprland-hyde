3d8c2c59566ef4089bf80c6c8d051a8a75ae23dd
Lassi Haapala <lassi.p.haapala@gmail.com>
2025-04-25 14:53:31 +0300

    0.1.6

---
+++ modules/hm/node.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  home.packages = with pkgs; [
+    # Node.js - latest stable
+    nodejs
+    
+    # Package managers
+    nodePackages.npm
+    nodePackages.pnpm
+    yarn
+    bun  # Bun runtime and package manager
+    
+    # TypeScript and related tools
+    nodePackages.typescript
+    nodePackages.ts-node
+    nodePackages.typescript-language-server
+    
+    # JS/TS linting and formatting
+    nodePackages.eslint
+    nodePackages.prettier
+  ];
+
+  # Add Node version management
+  programs.nvm = {
+    enable = true;
+  };
+}
